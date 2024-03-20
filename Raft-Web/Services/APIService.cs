7b95d07518c6a4105026cc97d0fd27ee93ac0528
Zackery Anderson <zackatack108@gmail.com>
2024-03-19 23:26:10 -0600

    Got user and product accounts added

---
+++ Raft-Web/Services/APIService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+namespace Services.Services;
+
+public class APIService
+{
+    private readonly HttpClient client;
+
+    public APIService(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task AddMoney(string name, decimal amount)
+    {
+        (string, decimal) usernameBalance = new(name, amount);
+        await client.PostAsJsonAsync($"Gateway/Add", usernameBalance);
+    }
+
+    public async Task<decimal> ViewBalance(string name)
+    {
+        return await client.GetFromJsonAsync<decimal>($"Gateway/Get?key={name}");
+    }
+
+    public async Task AddProduct(string name, int numItems)
+    {
+        (string, int) usernameBalance = new(name, numItems);
+        await client.PostAsJsonAsync($"Gateway/Add", usernameBalance);
+    }
+
+    public async Task UpdateProduct(string name, int numItems)
+    {
+        (string, int) usernameBalance = new(name, numItems);
+        await client.PostAsJsonAsync($"Gateway/Add", usernameBalance);
+    }
+}
